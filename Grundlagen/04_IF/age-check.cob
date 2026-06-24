838a3212257af9e4e8dca390867737efd42c252f
Speckamp <julia.speckamp@Speckamps-MacBook-Pro.local>
2026-06-24 18:59:59 +0200

    Verschachtelte Conditions hinzugefuegt

--- Grundlagen/04_IF/age-check.cob
+++ Grundlagen/04_IF/age-check.cob
@@ -6,13 +6,18 @@ working-storage section.
 01 age pic 999. 
 
 procedure division.
-       display "Bitte Alter eingeben".
+       display "Bitte Alter des Kunden eingeben".
        accept age.
 
-       if age >= 18
-           display "Entry granted"
-       else
-           display "Minor detected. Entry denied."
+       if age < 18
+           display "Kunde minderjährig: Kinderbeiträge"
+       *> Elseif sind hier else -> if und eingerückt. Jedes if und damit auch else if braucht ein end-if
+       else 
+           if age <= 66
+               display "Kunde im Erwerbsalter"
+           else
+               display "Kunde im Rentenalter."
+           end-if
        end-if
 
 goback. 
