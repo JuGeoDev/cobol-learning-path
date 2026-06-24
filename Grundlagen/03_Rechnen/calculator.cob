db02931cad0a984782c777f7a5e52251677393f3
Speckamp <julia.speckamp@Speckamps-MacBook-Pro.local>
2026-06-24 12:38:19 +0200

    Funktion für basic Addition von 2 Zahlen erstellt

---
+++ Grundlagen/03_Rechnen/calculator.cob
@@ -0,0 +1,22 @@
+identification division.
+program-id. calculator.
+
+data division.
+working-storage section.
+
+01 first-number pic 9(3).
+01 second-number pic 9(3).
+01 result pic 9(4).
+
+procedure division.
+       display "Erste Zahl eingeben".
+       accept first-number.
+       display "Zweite Zahl eingeben".
+       accept second-number.
+
+       add first-number to second-number giving result.
+
+       display "Ergebnis:" result.
+
+       goback. 
+
