9b7a2400706e1db3597fc1677a59a0aa8ffdba85
JuGeoDev <jugeorg@proton.me>
2026-06-24 20:59:20 +0200

    Rechner mit Evaluate statt verschachtelten Conditionals

---
+++ Grundlagen/03_Rechnen/calculator-advanced-efficient.cob
@@ -0,0 +1,44 @@
+identification division.
+program-id. calculator-advanced-efficient.
+
+data division.
+working-storage section.
+
+01 operation pic x.
+01 first-number pic 9(4).
+01 second-number pic 9(4).
+*> s = signed = Vorzeichen, v99 = 2 Nachkommastellen = 2020 = 20,20
+01 result pic s9(8)v99.
+
+procedure division.
+
+       display "Welche Operation? (+,-,*,/)"
+       accept operation.
+
+       display "Erste Zahl eingeben".
+       accept first-number.
+
+       display "Zweite Zahl eingeben".
+       accept second-number.
+       *> Switch Case Äquivalent
+       evaluate true 
+           when operation = "+"
+               add first-number to second-number giving result
+           when operation = "-"
+               subtract second-number from first-number giving result
+           when operation = "*"
+               multiply first-number by second-number giving result
+           when operation = "/"
+               if second-number = 0
+                   display "Durch 0 teilen ist nicht erlaubt"
+                   goback 
+               end-if
+               divide first-number by second-number giving result
+           when other
+               display "Unbekannter Operator"
+               goback 
+       end-evaluate
+
+       display "Ergebnis:" result
+       goback. 
+
