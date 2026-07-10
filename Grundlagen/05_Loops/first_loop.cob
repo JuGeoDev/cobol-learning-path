9c41f9fdcf7376ce8a0edd0d07c59558d53cebfb
JuGeoDev <jugeorg@proton.me>
2026-07-10 19:15:28 +0200

    Lil refactoring. Check for numeric counter input now

--- Grundlagen/05_Loops/first_loop.cob
+++ Grundlagen/05_Loops/first_loop.cob
@@ -4,18 +4,24 @@ program-id. count-hello.
 data division.
 working-storage section.
 
-01 counter pic 9(1).
+01 counter pic 9(3) value 0.
+01 counter-input pic x(3).
 
 procedure division.
-       display "Wie oft soll ich hallo sagen?".
-       accept counter.
+       display "Wie oft soll ich hallo sagen?"
+       accept counter-input
+       if function trim(counter-input) is not numeric 
+           display "Ungueltige Eingabe"
+           goback
+       end-if
 
-    perform until counter = 0
-        display "Hello"
-        subtract 1 from counter
-    end-perform
+       move function trim(counter-input) to counter
 
-    display "Fertig"
+       perform until counter = 0
+           display "Hello"
+           subtract 1 from counter
+       end-perform
 
-    goback.
-    
+       display "Fertig"
+
+       goback.
